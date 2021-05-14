8cab0951ae33f32a21b4a4b5b3e27955ee739d7d
Selom <selomk@hotmail.com>
2021-05-14 07:27:27 -0500

    Earthquakes and tectonicplates urls

---
+++ Leaflet-Step-2/static/js/logic.js
@@ -0,0 +1,5 @@
+console.log("logic.js loaded");
+
+// GeoJSON url variables
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicplatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
