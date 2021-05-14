85ccde58c79965e07a395490d30c8b54b35356a6
Selom <selomk@hotmail.com>
2021-05-14 07:46:21 -0500

    Legend

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -113,3 +113,21 @@ function chooseColor(depth) {
     }).addTo(tectonicplates);
     tectonicplates.addTo(myMap);
   });
+
+   // Legend
+   var legend = L.control({position: "bottomright"});
+   legend.onAdd = function() {
+     var div = L.DomUtil.create("div", "info legend"),
+     depth = [-10, 10, 30, 50, 70, 90];
+     
+   //   div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+ 
+     for (var i =0; i < depth.length; i++) {
+       div.innerHTML += 
+       '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+           depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+       }
+       return div;
+     };
+     legend.addTo(myMap);
+});
